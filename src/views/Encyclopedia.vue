b7b718f4f4674dcd7f9e6dfa6bd28f5d8a3f06fb
Maciej Małota <maciejmny@gmail.com>
2021-11-23 14:23:50 +0100

    Encyclo

---
+++ src/views/Encyclopedia.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="container">
+          <!-- <button
+            type="button"
+            class="btn btn-success"
+          
+          >
+            DODAJ GRE
+          </button> -->
+
+  <div class="card text-white bg-dark mb-3 ">
+        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+      <img   class="card-img-top " src="https://cdn.segmentnext.com/wp-content/uploads/2021/09/God-of-War-Ragnarok-Freya-Poster.jpg" />
+      <a href="#!">
+        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+    </a>
+  </div>
+
+  <div class="card-body">
+      <h3 class="card-title">God of War</h3>
+      <button type="button" class="btn btn-primary" id="btn2">Czytaj</button>
+    </div>
+  </div>
+
+
+   <div class="card text-white bg-dark mb-3 ">
+        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+      <img   class="card-img-top " src="https://cdn.segmentnext.com/wp-content/uploads/2021/09/God-of-War-Ragnarok-Freya-Poster.jpg" />
+      <a href="#!">
+        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+    </a>
+  </div>
+
+  <div class="card-body">
+      <h3 class="card-title">God of War</h3>
+      <button type="button" class="btn btn-primary" id="btn2">Czytaj</button>
+    </div>
+  </div>
+
+     <div class="card text-white bg-dark mb-3 ">
+        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+      <img   class="card-img-top " src="https://cdn.segmentnext.com/wp-content/uploads/2021/09/God-of-War-Ragnarok-Freya-Poster.jpg" />
+      <a href="#!">
+        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+    </a>
+  </div>
+
+  <div class="card-body">
+      <h3 class="card-title">God of War</h3>
+      <button type="button" class="btn btn-primary" id="btn2">Czytaj</button>
+    </div>
+  </div>
+
+     <div class="card text-white bg-dark mb-3 ">
+        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+      <img   class="card-img-top " src="https://cdn.segmentnext.com/wp-content/uploads/2021/09/God-of-War-Ragnarok-Freya-Poster.jpg" />
+      <a href="#!">
+        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+    </a>
+  </div>
+
+  <div class="card-body">
+      <h3 class="card-title">God of War</h3>
+      <button type="button" class="btn btn-primary" id="btn2">Czytaj</button>
+    </div>
+  </div>
+
+       <div class="card text-white bg-dark mb-3 ">
+        <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+      <img   class="card-img-top " src="https://cdn.segmentnext.com/wp-content/uploads/2021/09/God-of-War-Ragnarok-Freya-Poster.jpg" />
+      <a href="#!">
+        <div class="mask" style="background-color: rgba(251, 251, 251, 0.15)"></div>
+    </a>
+  </div>
+
+  <div class="card-body">
+      <h3 class="card-title">God of War</h3>
+      <button type="button" class="btn btn-primary" id="btn2">Czytaj</button>
+    </div>
+  </div>
+
+
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+
+.card{
+ margin: auto;
+ height: 28rem;
+  text-align: center;
+  width: 18rem;
+  background: #858585b3;
+  display:inline-block !important;
+  margin: 40;
+  margin-left: 100px;
+  margin-top: 20px;
+}
+.card-body{
+  
+}
+
+.card img{
+  
+width: 250px;
+	height:300px;
+	margin:10px;
+  
+  
+	
+}
+ /* .card-img-top {
+ width: 100%;
+    height: 15vw;
+    object-fit: cover;
+    
+ 
+    
+}  */
+#btn2{
+
+   color: white;
+  background: crimson;
+}
+
+</style>
